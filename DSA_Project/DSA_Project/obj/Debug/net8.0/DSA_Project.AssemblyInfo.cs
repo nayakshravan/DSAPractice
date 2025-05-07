b019c5d0449c6e87a6ac59c3abde9b9a09b82845
snayak <nayak.shravan07@gmail.com>
2025-05-07 15:37:43 +0530

    Clean up build and IDE-Specific files

--- DSA_Project/DSA_Project/obj/Debug/net8.0/DSA_Project.AssemblyInfo.cs
+++ DSA_Project/DSA_Project/obj/Debug/net8.0/DSA_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DSA_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36b98b8e531ff748a1b55243d066f1fd9e7ecbba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ec5a67999f7585c2f3c5807834a647d484baf90")]
 [assembly: System.Reflection.AssemblyProductAttribute("DSA_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DSA_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
